1222a1052c31309d1fe6f073cb780164d0598476
kerjaanlucky <kerjaanlucky@gmail.com>
2024-07-20 23:01:11 +0700

    text

--- src/views/gudang/Mutasi/Keluar/KeluarScreen.vue
+++ src/views/gudang/Mutasi/Keluar/KeluarScreen.vue
@@ -39,7 +39,12 @@
                 <span class="label-text">No Pol Kendaraan</span>
               </label>
               <div class="flex items-center">
-                <input v-model="mutationStore.exitItem.data.no_pol" type="text" placeholder="Type here" class="input input-bordered w-full" />
+                <input
+                  v-model="mutationStore.exitItem.data.no_pol"
+                  type="text"
+                  placeholder="Type here"
+                  class="input input-bordered w-full"
+                />
               </div>
             </div>
             <div class="form-control">
@@ -47,7 +52,12 @@
                 <span class="label-text">Supir</span>
               </label>
               <div class="flex items-center">
-                <input v-model="mutationStore.exitItem.data.driver_name" type="text" placeholder="Type here" class="input input-bordered w-full" />
+                <input
+                  v-model="mutationStore.exitItem.data.driver_name"
+                  type="text"
+                  placeholder="Type here"
+                  class="input input-bordered w-full"
+                />
               </div>
             </div>
             <div class="form-control">
@@ -55,13 +65,28 @@
                 <span class="label-text">Admin</span>
               </label>
               <div class="flex items-center">
-                <input v-model="mutationStore.exitItem.data.admin_name" type="text" placeholder="Type here" class="input input-bordered w-full" />
+                <input
+                  v-model="mutationStore.exitItem.data.admin_name"
+                  type="text"
+                  placeholder="Type here"
+                  class="input input-bordered w-full"
+                />
               </div>
             </div>
             <div class="card-actions justify-end my-4">
-              <button :disabled="mutationStore.storeLoading" type="submit" class="btn btn-secondary modal-button">
+              <button
+                :disabled="mutationStore.storeLoading"
+                type="submit"
+                class="btn btn-secondary modal-button"
+              >
                 <template v-if="mutationStore.storeLoading">
-                  <svg role="status" class="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <svg
+                    role="status"
+                    class="inline mr-3 w-4 h-4 text-white animate-spin"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path
                       d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
                       fill="#E5E7EB"
@@ -86,7 +111,11 @@
             <p>Isi dengan data Item yang keluar ke persediaan</p>
 
             <div class="card-actions justify-end">
-              <label for="my-modal-item" class="btn w-32 btn-secondary modal-button shadow-md"><span class="text-xs">Tambah</span></label>
+              <label
+                for="my-modal-item"
+                class="btn w-32 btn-secondary modal-button shadow-md"
+                ><span class="text-xs">Tambah</span></label
+              >
             </div>
 
             <div class="flex mt-2 md:overflow-visible overflow-y-auto mb-5">
@@ -108,16 +137,37 @@
                       <span>Tidak ada data.</span>
                     </td>
                   </tr>
-                  <tr v-else v-for="(item, index) in mutationStore.exitItem.detail" :key="item">
+                  <tr
+                    v-else
+                    v-for="(item, index) in mutationStore.exitItem.detail"
+                    :key="item"
+                  >
                     <td class="text-center"></td>
                     <td>{{ item.name.toUpperCase() }}</td>
                     <td>{{ item.unit.name.toUpperCase() }}</td>
                     <td>
-                      <input v-model="item.qty" type="number" placeholder="0" min="0" class="input input-bordered input-sm w-1/2 max-w-xs" />
+                      <input
+                        v-model="item.qty"
+                        type="number"
+                        placeholder="0"
+                        min="0"
+                        class="input input-bordered input-sm w-1/2 max-w-xs"
+                      />
                     </td>
                     <td>
-                      <button class="btn btn-sm btn-square btn-outline" @click="deleteData(index)">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" width="24" height="24" stroke="currentColor">
+                      <button
+                        class="btn btn-sm btn-square btn-outline"
+                        @click="deleteData(index)"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          width="24"
+                          height="24"
+                          stroke="currentColor"
+                        >
                           <path
                             stroke-linecap="round"
                             stroke-linejoin="round"
@@ -141,10 +191,10 @@
 
 <script>
 import { useMutationStore, useAuthStore } from '@/stores/store'
-import ModalItem from './component/ModalItem.vue'
 import { useToast } from 'vue-toastification'
 import { inject } from 'vue'
 import { useRouter } from 'vue-router'
+import ModalItem from './component/ModalItem.vue'
 
 export default {
   setup() {
