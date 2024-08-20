36675044fdf5fde6a5e7e0546b46c148de09af4b
LuckyAnggara <anggara.lucky1992@gmail.com>
2024-08-20 09:48:58 +0700

    test

--- src/views/LoginScreen.vue
+++ src/views/LoginScreen.vue
@@ -1,12 +1,7 @@
 <template>
   <section class="bg-gray-50 dark:bg-gray-900">
-    <div
-      class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
-    >
-      <a
-        href="#"
-        class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-      >
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
         <!-- <img
           class="w-8 h-8 mr-2"
           src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
@@ -14,22 +9,12 @@
         /> -->
         Berkah Baja Makmur
       </a>
-      <div
-        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
-      >
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1
-            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
-          >
-            Masuk ke Akun mu
-          </h1>
+          <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Masuk ke Akun mu</h1>
           <form class="space-y-4 md:space-y-6" action="#">
             <div>
-              <label
-                for="username"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >User</label
-              >
+              <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">User</label>
               <input
                 v-model="authStore.username"
                 type="username"
@@ -40,17 +25,12 @@
               />
             </div>
             <div>
-              <label
-                for="password"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >Password</label
-              >
+              <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
               <input
                 v-model="authStore.password"
                 type="password"
                 name="password"
                 id="password"
-                placeholder="••••••••"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 required=""
               />
@@ -63,13 +43,7 @@
               class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               <template v-if="authStore.isLoading">
-                <svg
-                  role="status"
-                  class="inline mr-3 w-4 h-4 text-white animate-spin"
-                  viewBox="0 0 100 101"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
+                <svg role="status" class="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path
                     d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
                     fill="#E5E7EB"
--- src/views/bisnis/penjualan/PenjualanNew.vue
+++ src/views/bisnis/penjualan/PenjualanNew.vue
@@ -7,12 +7,7 @@
 
           <div class="form-control w-52">
             <label class="cursor-pointer label">
-              <input
-                type="checkbox"
-                checked="checked"
-                class="checkbox checkbox-primary"
-                v-model="penjualanStore.form.pelanggan_tetap"
-              />
+              <input type="checkbox" checked="checked" class="checkbox checkbox-primary" v-model="penjualanStore.form.pelanggan_tetap" />
 
               <!-- <input
                   type="checkbox"
@@ -26,35 +21,17 @@
             <div class="flex flex-col space-y-2">
               <label class="input input-bordered flex items-center gap-2">
                 <UserIcon class="h-4 w-4 opacity-70" />
-                <input
-                  required
-                  v-model="penjualanStore.form.nama_pelanggan"
-                  type="text"
-                  class="grow input-sm"
-                  placeholder="Nama Pelanggan"
-                />
+                <input required v-model="penjualanStore.form.nama_pelanggan" type="text" class="grow input-sm" placeholder="Nama Pelanggan" />
               </label>
 
               <label class="input input-bordered flex items-center gap-2">
                 <UserIcon class="h-4 w-4 opacity-70" />
-                <input
-                  required
-                  v-model="penjualanStore.form.alamat"
-                  type="text"
-                  class="grow input-sm"
-                  placeholder="Alamat Pelanggan"
-                />
+                <input required v-model="penjualanStore.form.alamat" type="text" class="grow input-sm" placeholder="Alamat Pelanggan" />
               </label>
 
               <label class="input input-bordered flex items-center gap-2">
                 <PhoneIcon class="h-4 w-4 opacity-70" />
-                <input
-                  required
-                  v-model="penjualanStore.form.nomor_telepon"
-                  type="text"
-                  class="grow input-sm"
-                  placeholder="Nomor Telepon"
-                />
+                <input required v-model="penjualanStore.form.nomor_telepon" type="text" class="grow input-sm" placeholder="Nomor Telepon" />
               </label>
             </div>
           </template>
@@ -71,20 +48,12 @@
                     class="grow input-sm"
                   />
 
-                  <ul
-                    v-if="pelangganStore.items.length > 0"
-                    tabindex="0"
-                    class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-full"
-                  >
+                  <ul v-if="pelangganStore.items.length > 0" tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-full">
                     <li v-for="item in pelangganStore.items">
                       <a @click="setPelanggan(item)">{{ item.name }}</a>
                     </li>
                   </ul>
-                  <ul
-                    v-else
-                    tabindex="0"
-                    class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-full"
-                  >
+                  <ul v-else tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-full">
                     <li><a>Tidak ada data, silahkan cari Item lain</a></li>
                   </ul>
                 </div>
@@ -93,24 +62,12 @@
 
               <label class="input input-bordered flex items-center gap-2">
                 <UserIcon class="h-4 w-4 opacity-70" />
-                <input
-                  required
-                  v-model="penjualanStore.form.pelanggan.alamat"
-                  type="text"
-                  class="grow input-sm"
-                  placeholder="Alamat Pelanggan"
-                />
+                <input required v-model="penjualanStore.form.pelanggan.alamat" type="text" class="grow input-sm" placeholder="Alamat Pelanggan" />
               </label>
 
               <label class="input input-bordered flex items-center gap-2">
                 <PhoneIcon class="h-4 w-4 opacity-70" />
-                <input
-                  required
-                  v-model="penjualanStore.form.pelanggan.nomor_telepon"
-                  type="text"
-                  class="grow input-sm"
-                  placeholder="Nomor Telepon"
-                />
+                <input required v-model="penjualanStore.form.pelanggan.nomor_telepon" type="text" class="grow input-sm" placeholder="Nomor Telepon" />
               </label>
             </div>
           </template>
@@ -121,40 +78,21 @@
           <div class="flex flex-col space-y-2">
             <label class="input input-bordered flex items-center gap-2">
               <CalendarDaysIcon class="h-4 w-4 opacity-70" />
-              <input
-                required
-                v-model="penjualanStore.form.tanggal_transaksi"
-                type="text"
-                class="grow input-sm"
-                placeholder="Tanggal Transaksi"
-              />
+              <input required v-model="penjualanStore.form.tanggal_transaksi" type="text" class="grow input-sm" placeholder="Tanggal Transaksi" />
             </label>
 
             <label class="input input-bordered flex items-center gap-2">
               <KeyIcon class="h-4 w-4 opacity-70" />
-              <input
-                required
-                v-model="penjualanStore.form.nomor_faktur"
-                type="text"
-                class="grow input-sm"
-                placeholder="Nomor Faktur"
-              />
+              <input required v-model="penjualanStore.form.nomor_faktur" type="text" class="grow input-sm" placeholder="Nomor Faktur" />
             </label>
 
             <div class="form-control w-full flex flex-row space-x-4">
               <div class="label w-1/5">
                 <span class="">Sales </span>
               </div>
-              <select
-                class="select select-bordered w-4/5"
-                v-model="penjualanStore.form.sales_id"
-              >
+              <select class="select select-bordered w-4/5" v-model="penjualanStore.form.sales_id">
                 <option :value="0">Tidak ada</option>
-                <option
-                  v-for="(sales, index) in penjualanStore.dataSales"
-                  :key="index"
-                  :value="sales.id"
-                >
+                <option v-for="(sales, index) in penjualanStore.dataSales" :key="index" :value="sales.id">
                   {{ sales.name }}
                 </option>
               </select>
@@ -167,29 +105,14 @@
 
           <label class="input input-bordered flex items-center gap-2">
             <div class="dropdown dropdown-hover w-full">
-              <input
-                v-model="itemStore.searchName"
-                tabindex="0"
-                type="text"
-                @keyup="searchData"
-                placeholder="Cari Data Barang"
-                class="grow input-sm"
-              />
+              <input v-model="itemStore.searchName" tabindex="0" type="text" @keyup="searchData" placeholder="Cari Data Barang" class="grow input-sm" />
 
-              <ul
-                v-if="itemStore.items.length > 0"
-                tabindex="0"
-                class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-full"
-              >
+              <ul v-if="itemStore.items.length > 0" tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-full">
                 <li v-for="(item, index) in itemStore.items" :key="index">
                   <a @click="penjualanStore.addCart(item)">{{ item.name }}</a>
                 </li>
               </ul>
-              <ul
-                v-else
-                tabindex="0"
-                class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-full"
-              >
+              <ul v-else tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-full">
                 <li><a>Tidak ada data, silahkan cari Item lain</a></li>
               </ul>
             </div>
@@ -212,7 +135,7 @@
                 <th>Nama Item</th>
                 <th>Kuantitas</th>
                 <th>Satuan</th>
-                <th>Harga Pokok</th>
+                <!-- <th>Harga Pokok</th> -->
                 <th>Harga Jual</th>
                 <th>Discount</th>
                 <th>Total (IDR)</th>
@@ -222,64 +145,42 @@
 
             <tbody>
               <tr v-if="penjualanStore.form.cart.length == 0">
-                <td colspan="9" class="text-center">Tidak ada data</td>
+                <td colspan="8" class="text-center">Tidak ada data</td>
               </tr>
-              <tr
-                v-else
-                v-for="(item, index) in penjualanStore.form.cart"
-                :key="index"
-              >
+              <tr v-else v-for="(item, index) in penjualanStore.form.cart" :key="index">
                 <td class="text-center">
                   {{ 1 + index }}
                 </td>
                 <td>{{ item.name }}</td>
                 <td>
-                  <input
-                    v-model="item.jumlah"
-                    type="text"
-                    placeholder="Jumlah Pembelian"
-                    class="input input-bordered w-full max-w-xs"
-                  />
+                  <input v-model="item.jumlah" type="text" placeholder="Jumlah Pembelian" class="input input-bordered w-full max-w-xs" />
                 </td>
                 <td>
                   {{ item.unit?.name }}
                 </td>
-                <td>
+                <!-- <td>
                   <InputCurrency
                     v-model="item.cogs"
                     :options="{ currency: 'IDR' }"
                   />
+                </td> -->
+                <td>
+                  <InputCurrency v-model="item.harga" :options="{ currency: 'IDR' }" />
                 </td>
                 <td>
-                  <InputCurrency
-                    v-model="item.harga"
-                    :options="{ currency: 'IDR' }"
-                  />
-                </td>
-                <td>
-                  <InputCurrency
-                    v-model="item.discount"
-                    :options="{ currency: 'IDR' }"
-                  />
+                  <InputCurrency v-model="item.discount" :options="{ currency: 'IDR' }" />
                 </td>
                 <td>
                   <input
                     readonly
-                    :value="
-                      numeral(item.harga * item.jumlah - item.discount).format(
-                        '0,0'
-                      )
-                    "
+                    :value="numeral(item.harga * item.jumlah - item.discount).format('0,0')"
                     type="text"
                     placeholder="Jumlah Pembelian"
                     class="input input-bordered w-full max-w-xs"
                   />
                 </td>
                 <td>
-                  <TrashIcon
-                    @click="removeItem(index)"
-                    class="w-6 h-6 cursor-pointer transition duration-150 ease-in-out hover:scale-120"
-                  />
+                  <TrashIcon @click="removeItem(index)" class="w-6 h-6 cursor-pointer transition duration-150 ease-in-out hover:scale-120" />
                 </td>
               </tr>
             </tbody>
@@ -305,66 +206,33 @@
             <div class="label w-2/5">
               <span class="">Total (IDR) </span>
             </div>
-            <input
-              required
-              readonly
-              type="text"
-              :value="numeral(penjualanStore.cartTotal).format('0,0')"
-              class="input input-bordered input-sm w-3/5"
-            />
+            <input required readonly type="text" :value="numeral(penjualanStore.cartTotal).format('0,0')" class="input input-bordered input-sm w-3/5" />
           </div>
 
           <div class="form-control w-full flex flex-row space-x-4">
             <div class="label w-2/5">
               <span class="">Pajak (IDR) </span>
             </div>
-            <select
-              v-model="penjualanStore.tipePajak"
-              class="select select-bordered w-2/5 select-sm"
-            >
+            <select v-model="penjualanStore.tipePajak" class="select select-bordered w-2/5 select-sm">
               <option value="0" selected>Tanpa Pajak</option>
               <option value="11">PPN 11%</option>
             </select>
-            <input
-              required
-              readonly
-              type="text"
-              :value="numeral(penjualanStore.pajakTotal).format('0,0')"
-              class="input input-bordered w-full input-sm"
-            />
+            <input required readonly type="text" :value="numeral(penjualanStore.pajakTotal).format('0,0')" class="input input-bordered w-full input-sm" />
           </div>
           <div class="form-control w-full flex-row">
             <div class="label w-2/5">
               <span class="">Ongkos Kirim (IDR)</span>
             </div>
-            <input
-              required
-              type="text"
-              v-model="penjualanStore.form.ongkir"
-              class="input input-bordered input-sm w-3/5"
-            />
+            <input required type="text" v-model="penjualanStore.form.ongkir" class="input input-bordered input-sm w-3/5" />
           </div>
           <div class="form-control w-full flex-row">
             <div class="label w-2/5">
               <span class="">Grand Total (IDR)</span>
             </div>
-            <input
-              required
-              readonly
-              :value="numeral(penjualanStore.grandTotal).format('0,0')"
-              type="text"
-              class="input input-bordered input-sm w-3/5"
-            />
+            <input required readonly :value="numeral(penjualanStore.grandTotal).format('0,0')" type="text" class="input input-bordered input-sm w-3/5" />
           </div>
-          <button
-            type="submit"
-            :disabled="penjualanStore.isStoreLoading"
-            class="mt-6 btn btn-accent w-32 hover:btn-primary my-2"
-          >
-            <span
-              v-if="penjualanStore.isStoreLoading"
-              class="loading loading-infinity loading-lg"
-            ></span>
+          <button type="submit" :disabled="penjualanStore.isStoreLoading" class="mt-6 btn btn-accent w-32 hover:btn-primary my-2">
+            <span v-if="penjualanStore.isStoreLoading" class="loading loading-infinity loading-lg"></span>
             <span v-else>Submit</span>
           </button>
         </form>
@@ -383,14 +251,7 @@ import { useMainStore } from '@/stores/mainStore'
 import { useDebounceFn } from '@vueuse/core'
 import { useRouter } from 'vue-router'
 import { useItemStore } from '@/stores/store'
-import {
-  TrashIcon,
-  MagnifyingGlassIcon,
-  UserIcon,
-  PhoneIcon,
-  CalendarDaysIcon,
-  KeyIcon,
-} from '@heroicons/vue/24/solid'
+import { TrashIcon, MagnifyingGlassIcon, UserIcon, PhoneIcon, CalendarDaysIcon, KeyIcon } from '@heroicons/vue/24/solid'
 import { useToast } from 'vue-toastification'
 import numeral from 'numeral'
 const swal = inject('$swal')
