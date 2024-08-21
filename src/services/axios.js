2c0a034c9732bd4dd4ca5e00e92ce21a0f527ee2
LuckyAnggara <anggara.lucky1992@gmail.com>
2024-08-21 09:31:52 +0700

    ok

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const baseUrlLink = 'http://127.0.0.1:8000/'
+export const baseUrlLink = 'https://api-pabrik.bbmakmur.com/'
 
 const axiosIns = axios.create({
   // baseURL: 'http://127.0.0.1:8000/api/',
